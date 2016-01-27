3b7bf79f736e906d7fe82ca919bbef447a89c3df
TANIGUCHI Takaki <takaki@asis.media-as.org>
2016-01-27 19:58:45 +0900

    Spock: dip.order.Build

---
+++ src/test/groovy/dip/order/BuildTest.groovy
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2016 TANIGUCHI Takaki
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package dip.order
+
+import dip.world.Coast
+import dip.world.Location
+import dip.world.Power
+import dip.world.Province
+import dip.world.Unit
+import spock.lang.Specification
+
+class BuildTest extends Specification {
+    static def power = new Power(["France"] as String[], "French", true)
+    static def location = new Location(new Province("Spain", ["spa"] as String[], 0, false), Coast.LAND)
+
+    def "illegal arguments throw a exception"() {
+        when:
+        new Build(pw, src, unit)
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        pw    | src      | unit
+        null  | location | Unit.Type.FLEET
+        power | null     | Unit.Type.FLEET
+        power | location | null
+    }
+
+    def "getter"() {
+        def build = new Build(power, location, Unit.Type.ARMY)
+        expect:
+        build.getBriefName() == "B"
+        build.getFullName() == "Build"
+        build.getDefaultFormat() == "{getPower()}: {_orderName_} {getSourceUnitType()} {getSource()}"
+        build.toBriefString() == "France: B A spa"
+        build.toFullString() == "France: Build Army Spain"
+        build.equals(new Build(power, location, Unit.Type.ARMY))
+
+    }
+
+}
