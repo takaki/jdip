d2de8b12a5d8b5608cd59ba57b2062960fe60198
TANIGUCHI Takaki <takaki@asis.media-as.org>
2016-03-02 15:08:10 +0900

    array to List

--- src/main/java/dip/gui/dialog/MetadataDialog.java
+++ src/main/java/dip/gui/dialog/MetadataDialog.java
@@ -42,6 +42,7 @@ import javax.swing.border.EtchedBorder;
 import java.awt.*;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 /**
  * Metadata Editing/Viewing dialog
@@ -384,7 +385,7 @@ public class MetadataDialog extends HeaderDialog {
             name.setText(pmd.getName());
             uri.setText(n2e(pmd.getURI()));
 
-            String[] tmpEmail = pmd.getEmailAddresses();
+            String[] tmpEmail = pmd.getEmailAddresses().toArray(new String[0]);
             for (int i = 0; i < email.length; i++) {
                 if (i < tmpEmail.length) {
                     email[i].setText(n2e(tmpEmail[i]));
@@ -407,7 +408,7 @@ public class MetadataDialog extends HeaderDialog {
                 tmpEmail[i] = email[i].getText().trim();
             }
 
-            pmd.setEmailAddresses(tmpEmail);
+            pmd.setEmailAddresses(Arrays.asList(tmpEmail));
         }// write()
     }// inner class PlayerPanel
 
